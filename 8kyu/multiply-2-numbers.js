ddf31ec4dac9bd55c08111a677a8c11123322800
Ashley Famularo <ash0fam@gmail.com>
2022-09-05 10:27:43 -0400

    added new codewars solution

---
+++ 8kyu/multiply-2-numbers.js
@@ -0,0 +1,17 @@
+// Description:
+
+// Implement a function called multiply, which takes two numbers and returns their product:
+
+// multiply(2, 3) = 6
+// multiply(0, 188) = 0
+// multiply(85, 144) = 12240
+
+
+
+// Write here your multiply-function
+let multiply = (a,b) => a*b;
+
+// Write here your multiply-function
+// function multiply(x,y){
+//   return x*y
+// }
