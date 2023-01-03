ff51c30567c696a0c7f38366f6438e853063222c
Ashley Famularo <ash0fam@gmail.com>
2023-01-02 23:03:14 -0500

    Add new Leetcode solution

---
+++ leetcode/876.js
@@ -0,0 +1,49 @@
+// Given the head of a singly linked list, return the middle node of the linked list.
+
+// If there are two middle nodes, return the second middle node.
+
+//  
+
+// Example 1:
+
+// Input: head = [1,2,3,4,5]
+// Output: [3,4,5]
+// Explanation: The middle node of the list is node 3.
+
+// Example 2:
+
+// Input: head = [1,2,3,4,5,6]
+// Output: [4,5,6]
+// Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+//  
+
+// Constraints:
+
+//     The number of nodes in the list is in the range [1, 100].
+//     1 <= Node.val <= 100
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ /* @param {ListNode} head
+ /* @return {ListNode}
+ */
+let middleNode = function(head){
+  let size = 0;
+  let arr = head;
+  while (arr) {
+    arr = arr.next;
+    size++;
+  }
+  let middle = size % 2 === 0 ? size / 2 + 1 : Math.ceil(size / 2);
+  arr = head;
+  while (--middle) arr = arr.next;
+  return arr;
+};
